29e8d1229465cb7fcf7242529c08c11130420d01
Bsab1 <begim2017@gmail.com>
2023-03-15 12:38:39 -0500

    initial commit

---
+++ src/test/java/tests/Phone.java
@@ -0,0 +1,12 @@
+package tests;
+
+import utulities.Driver;
+
+public class Phone {
+
+    public static void main(String[] args) {
+        Driver.getDriver().get("https://google.com");
+
+
+    }
+}
